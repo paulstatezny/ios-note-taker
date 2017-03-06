284f58db1e1a99b4b56e2f740eb6953543218f5b
Paul Statezny <paulstatezny@gmail.com>
2017-03-06 15:59:18 -0700

    Initial Commit

---
+++ NoteTaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoteTaker
+//
+//  Created by Paul Statezny on 3/6/17.
+//  Copyright © 2017 Paul Statezny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
