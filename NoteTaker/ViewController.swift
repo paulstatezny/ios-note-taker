2ee770851cfdf4f240a4f1a5bb922efecaf6483b
Paul Statezny <paulstatezny@gmail.com>
2017-03-07 10:47:00 -0700

    Complete initial version of app with saving

--- NoteTaker/ViewController.swift
+++ NoteTaker/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     @IBOutlet weak var table: UITableView!
     var data:[String] = []
     var file:String!
+    var selectedRow:Int = -1
+    var newRowText:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,22 +27,40 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         
         let docsDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .allDomainsMask, true)
         
-        file = docsDir[0].appending("notes.txt")
+        file = docsDir[0].appending("/notes.txt")
         
         load()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if selectedRow == -1 {
+            return
+        }
+        
+        data[selectedRow] = newRowText
+        
+        if newRowText == "" {
+            data.remove(at: selectedRow)
+        }
+        
+        table.reloadData()
+        save()
     }
     
     func addNote() {
         if (table.isEditing) {
             return
         }
-        let name:String = "Row \(data.count + 1)"
+        let name:String = ""
         
         data.insert(name, at: 0)
         let indexPath:IndexPath = IndexPath(row: 0, section: 0)
         table.insertRows(at: [indexPath], with: .automatic)
+        table.selectRow(at: indexPath, animated: true, scrollPosition: .none)
         
-        save()
+        self.performSegue(withIdentifier: "detail", sender: nil)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -68,7 +88,15 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("\(data[indexPath.row])")
+        self.performSegue(withIdentifier: "detail", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let detailView:DetailViewController = segue.destination as! DetailViewController
+        
+        selectedRow = table.indexPathForSelectedRow!.row
+        detailView.masterView = self
+        detailView.setText(t: data[selectedRow])
     }
     
     func save() {
