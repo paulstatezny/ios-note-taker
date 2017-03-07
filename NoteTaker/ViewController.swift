58e92ae40475d4c294bb55273494d81388f40636
Paul Statezny <paulstatezny@gmail.com>
2017-03-06 21:35:41 -0700

    Implement basic view and saving features for note taking app

--- NoteTaker/ViewController.swift
+++ NoteTaker/ViewController.swift
@@ -8,11 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource {
 
+    @IBOutlet weak var table: UITableView!
+    var data:[String] = []
+    var file:String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Notes"
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNote))
+        self.navigationItem.rightBarButtonItem = addButton
+        self.navigationItem.leftBarButtonItem = editButtonItem
+        
+        let docsDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .allDomainsMask, true)
+        
+        file = docsDir[0].appending("notes.txt")
+        
+        load()
+    }
+    
+    func addNote() {
+        if (table.isEditing) {
+            return
+        }
+        let name:String = "Row \(data.count + 1)"
+        
+        data.insert(name, at: 0)
+        let indexPath:IndexPath = IndexPath(row: 0, section: 0)
+        table.insertRows(at: [indexPath], with: .automatic)
+        
+        save()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        cell.textLabel?.text = data[indexPath.row]
+        
+        return cell
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        table.setEditing(editing, animated: animated)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        data.remove(at: indexPath.row)
+        table.deleteRows(at: [indexPath], with: .fade)
+        
+        save()
+    }
+    
+    func save() {
+        // UserDefaults.standard.set(data, forKey: "notes")
+        // UserDefaults.standard.synchronize()
+        
+        let newData:NSArray = NSArray(array: data)
+        newData.write(toFile: file, atomically: true)
+    }
+    
+    func load() {
+        if let loadedData = NSArray(contentsOfFile: file) as? [String] {
+            data = loadedData
+            table.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
